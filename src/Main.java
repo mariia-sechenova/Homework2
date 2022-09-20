1df83af8ef82ea3be34f7a483ca82c2ec0f4f3a0
Mariia Sechenova <sechenova.m.a@yandex.ru>
2022-09-21 00:04:35 +0700

    5th task

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
 
-        System.out.println("task 4");
+        System.out.println("Task 4");
         var friend = 19;
         System.out.println(friend);
         friend += 2;
@@ -36,7 +36,7 @@ public class Main {
         System.out.println(friend);
 
         //task5
-        System.out.println("task 5");
+        System.out.println("Task 5");
         var frog = 3.5;
         System.out.println(frog);
         frog = 3.5 * 10;
@@ -46,6 +46,30 @@ public class Main {
         frog = frog + 4;
         System.out.println(frog);
 
+        //task6
+        System.out.println("Task 6");
+        var weightFirstBoxer = 78.2;
+        var weightSecondBoxer = 82.7;
+        var sumWeight = weightFirstBoxer + weightSecondBoxer;
+        System.out.println("Общий вес бойцов = " + sumWeight);
+        var subtractionWeight = weightSecondBoxer - weightFirstBoxer;
+        System.out.println("Разница между весами бойцов = " + subtractionWeight);
+
+        //task7
+        System.out.println("Task 7");
+        subtractionWeight = weightSecondBoxer % weightFirstBoxer;
+        System.out.println("Разница между весами бойцов = " + subtractionWeight);
+
+        //task8
+        System.out.println("Task 8");
+        var totalHours = 640;
+        var workingDay = 8;
+        var employees = totalHours / workingDay;
+        System.out.println("Всего работников в компании - " + employees + " человек ");
+
+        employees += 94;
+        workingDay = totalHours / employees;
+        System.out.println("Если в компании работает " + employees + " человек, то всего " + workingDay + " часов работы может быть поделено между сотрудниками");
     }
 }
 
