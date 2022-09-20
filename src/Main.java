aebfb2706d784cba147391e4d386ca05cd9fe45c
Mariia Sechenova <sechenova.m.a@yandex.ru>
2022-09-20 23:25:28 +0700

    5th task

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,26 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        System.out.println("task 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //task5
+        System.out.println("task 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = 3.5 * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
     }
 }
+
